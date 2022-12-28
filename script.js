e3bf8920ea853ae5b13f4ff32495b701e0692f96
stallionblade <119752878+stallionblade@users.noreply.github.com>
2022-12-28 14:46:12 -0500

    Add basic grid logic

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
 const container = document.querySelector('.gridcontainer')
+// container.style.minWidth = "50vw"
+let boxCount = 0
+let gridSize = 64
 
-const box = document.createElement('div')
-box.className = "box"
-box.style.backgroundColor = "red"
-// box.style.width = "40vw"
-box.style.height = "50px"
+while (boxCount < gridSize) {
+    const box = document.createElement('div')
+    box.className = "box"
+    box.style.backgroundColor = "red"
+    box.style.border = "0.5px blue solid"
+    box.style.minWidth = `calc(100vw/${Math.sqrt(gridSize) + 1})`
+    box.style.height = box.style.minWidth
 
-container.appendChild(box)
+    container.appendChild(box)
 
-const box2 = document.createElement('div')
-box2.className = "box"
-box2.style.backgroundColor = "blue"
-// box2.style.width = "40vw"
-box2.style.height = "50px"
+    boxCount++
+}
 
-container.appendChild(box2)
+// const box = document.createElement('div')
+// box.className = "box"
+// box.style.backgroundColor = "red"
+// // box.style.width = "40vw"
+// box.style.height = "50px"
 
-const box3 = document.createElement('div')
-box3.className = "box"
-box3.style.backgroundColor = "green"
-// box3.style.width = "40vw"
-box3.style.height = "50px"
+// container.appendChild(box)
 
-container.appendChild(box3)
+// const box2 = document.createElement('div')
+// box2.className = "box"
+// box2.style.backgroundColor = "blue"
+// // box2.style.width = "40vw"
+// box2.style.height = "50px"
 
-const box4 = document.createElement('div')
-box4.className = "box"
-box4.style.backgroundColor = "yellow"
-// box3.style.width = "40vw"
-box4.style.height = "50px"
+// container.appendChild(box2)
 
-container.appendChild(box4)
+// const box3 = document.createElement('div')
+// box3.className = "box"
+// box3.style.backgroundColor = "green"
+// // box3.style.width = "40vw"
+// box3.style.height = "50px"
+
+// container.appendChild(box3)
+
+// const box4 = document.createElement('div')
+// box4.className = "box"
+// box4.style.backgroundColor = "yellow"
+// // box3.style.width = "40vw"
+// box4.style.height = "50px"
+
+// container.appendChild(box4)
