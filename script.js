adfbf52c915e80529724f0db9fd74801380bf908
stallionblade <119752878+stallionblade@users.noreply.github.com>
2022-12-28 21:25:58 -0500

    Create dynamic grid - first pass

--- script.js
+++ script.js
@@ -1,49 +1,19 @@
 const container = document.querySelector('.gridcontainer')
-// container.style.minWidth = "50vw"
+
 let boxCount = 0
-let gridSize = 64
+let gridSize = 16
 
 while (boxCount < gridSize) {
     const box = document.createElement('div')
     box.className = "box"
     box.style.backgroundColor = "red"
-    box.style.border = "0.5px blue solid"
-    box.style.minWidth = `calc(100vw/${Math.sqrt(gridSize) + 1})`
-    box.style.height = box.style.minWidth
+    box.style.border = "0.25px white solid"
+    // box.style.minWidth = "calc(29.5*10px)"
+    box.style.minWidth = `calc(600px/${Math.sqrt(gridSize)}*0.95)`
+    
+    // box.style.height = box.style.minWidth
 
     container.appendChild(box)
 
     boxCount++
 }
-
-// const box = document.createElement('div')
-// box.className = "box"
-// box.style.backgroundColor = "red"
-// // box.style.width = "40vw"
-// box.style.height = "50px"
-
-// container.appendChild(box)
-
-// const box2 = document.createElement('div')
-// box2.className = "box"
-// box2.style.backgroundColor = "blue"
-// // box2.style.width = "40vw"
-// box2.style.height = "50px"
-
-// container.appendChild(box2)
-
-// const box3 = document.createElement('div')
-// box3.className = "box"
-// box3.style.backgroundColor = "green"
-// // box3.style.width = "40vw"
-// box3.style.height = "50px"
-
-// container.appendChild(box3)
-
-// const box4 = document.createElement('div')
-// box4.className = "box"
-// box4.style.backgroundColor = "yellow"
-// // box3.style.width = "40vw"
-// box4.style.height = "50px"
-
-// container.appendChild(box4)
