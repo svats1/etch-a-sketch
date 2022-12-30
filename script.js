af6760c03b202e53cb9dca489eba058f6fdc3ad3
stallionblade <119752878+stallionblade@users.noreply.github.com>
2022-12-29 20:56:11 -0500

    Create bi-directional DOM manip.

--- script.js
+++ script.js
@@ -1,25 +1,48 @@
-window.addEventListener('load', () => {
+// window.addEventListener('load', () => {
     
-    let boxCount = 0
+//     let boxCount = 0
     
-    const container = document.querySelector('.gridcontainer')
+//     const container = document.querySelector('.gridcontainer')
 
-    const input = document.querySelector('#input')
-    input.addEventListener('submit', e => {
-        e.preventDefault()
-        
-        let gridSize = parseInt(e.target[0].value)
+//     const input = document.querySelector('#input')
+//     input.addEventListener('submit', e => {
+//         e.preventDefault()
 
-        while (boxCount < gridSize) {
+//         while (boxCount < parseInt(e.target[0].value)) {
             
-            const box = document.createElement('div')
-            box.className = "box"
-            box.style.backgroundColor = "red"
-            box.style.border = "0.25px white solid"
+//             const box = document.createElement('div')
+//             box.className = "box"
+//             box.style.backgroundColor = "red"
+//             box.style.border = "0.25px white solid"
                     
-            container.appendChild(box)
+//             container.appendChild(box)
         
-            boxCount++
-        }
-    })
+//             boxCount++
+//         }
+//     })
+// })
+
+const input = document.querySelector('#input')
+input.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const container = document.querySelector('.gridcontainer')
+    container.innerHTML = ''
+
+    let boxCount = 0
+    while (boxCount < parseInt(e.target[0].value)) {
+
+        const box = document.createElement('div')
+  
+        box.className = "box"
+        box.style.backgroundColor = "red"
+        box.style.border = "0.25px white solid"
+                
+        container.appendChild(box)
+    
+        boxCount++
+    }
+
+    // Reset to re-enter While loop on next Submit
+    boxCount = 0
 })
