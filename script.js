0502250aa7a68e89a1df964dd6207bdcf362487d
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-01-02 16:13:29 -0500

    Add color picker

--- script.js
+++ script.js
@@ -1,29 +1,57 @@
-window.addEventListener('load', () => {
+// window.addEventListener('load', () => {
 
-    const input = document.querySelector('#input')
-    input.addEventListener('input', e => {
-        e.preventDefault()
-        const gridSize = parseInt(e.target.value)
+//     const input = document.querySelector('#input')
+//     input.addEventListener('input', e => {
+//         e.preventDefault()
+//         const gridSize = parseInt(e.target.value)
 
-        const container = document.querySelector('.gridcontainer')
-        container.innerHTML = ''
+//         const container = document.querySelector('.gridcontainer')
+//         container.innerHTML = ''
 
-        let boxCount = 0
-        while (boxCount < Math.pow(gridSize,2)) {
+//         let boxCount = 0
+//         while (boxCount < Math.pow(gridSize,2)) {
 
-            const box = document.createElement('div')    
-            box.className = "box"
-            box.style.backgroundColor = "white"
-            box.style.borderRadius = "3px"
-            box.style.minWidth = `calc(600px/${gridSize})`
+//             const box = document.createElement('div')    
+//             box.className = "box"
+//             box.style.backgroundColor = "white"
+//             box.style.borderRadius = "3px"
+//             box.style.minWidth = `calc(600px/${gridSize})`
                     
-            container.appendChild(box)
+//             container.appendChild(box)
 
-            boxCount++
-        }
+//             boxCount++
+//         }
 
-        // Reset to re-enter While loop on next Submit
-        boxCount = 0
+//         // Reset to re-enter While loop on next Submit
+//         boxCount = 0
+//     })
+
+// })
+
+
+const gridSize = 16
+const container = document.querySelector('.gridcontainer')
+const colorPicker = document.querySelector('#colorpicker')
+
+let boxCount = 0
+while (boxCount < Math.pow(gridSize, 2)) {
+
+    const box = document.createElement('div')    
+    box.className = "box"
+    box.style.backgroundColor = "white"
+    box.style.borderRadius = "3px"
+    box.style.minWidth = `calc(600px/${gridSize})`
+            
+    container.appendChild(box)
+
+    boxCount++
+
+    box.addEventListener('mouseenter', () => {
+        box.style.backgroundColor = colorPicker.value
     })
+}
 
-})
+// Reset to re-enter While loop on next Submit
+boxCount = 0
+
+
