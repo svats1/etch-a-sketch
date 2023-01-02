5cce880476764c9f462225b03cdd52805db1144d
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-01-02 16:42:19 -0500

    Refactor JS

--- script.js
+++ script.js
@@ -1,57 +1,32 @@
-// window.addEventListener('load', () => {
-
-//     const input = document.querySelector('#input')
-//     input.addEventListener('input', e => {
-//         e.preventDefault()
-//         const gridSize = parseInt(e.target.value)
-
-//         const container = document.querySelector('.gridcontainer')
-//         container.innerHTML = ''
-
-//         let boxCount = 0
-//         while (boxCount < Math.pow(gridSize,2)) {
-
-//             const box = document.createElement('div')    
-//             box.className = "box"
-//             box.style.backgroundColor = "white"
-//             box.style.borderRadius = "3px"
-//             box.style.minWidth = `calc(600px/${gridSize})`
-                    
-//             container.appendChild(box)
-
-//             boxCount++
-//         }
-
-//         // Reset to re-enter While loop on next Submit
-//         boxCount = 0
-//     })
-
-// })
-
-
-const gridSize = 16
-const container = document.querySelector('.gridcontainer')
+const input = document.querySelector('#input')
 const colorPicker = document.querySelector('#colorpicker')
+const container = document.querySelector('.gridcontainer')
 
-let boxCount = 0
-while (boxCount < Math.pow(gridSize, 2)) {
-
-    const box = document.createElement('div')    
-    box.className = "box"
-    box.style.backgroundColor = "white"
-    box.style.borderRadius = "3px"
-    box.style.minWidth = `calc(600px/${gridSize})`
-            
-    container.appendChild(box)
-
-    boxCount++
-
-    box.addEventListener('mouseenter', () => {
-        box.style.backgroundColor = colorPicker.value
-    })
+function drawGrid(gridSize) {
+    let boxCount = 0
+    while (boxCount < Math.pow(gridSize,2)) {
+        const box = document.createElement('div')    
+        box.className = "box"
+        box.style.backgroundColor = "white"
+        box.style.borderRadius = "3px"
+        box.style.minWidth = `calc(600px/${gridSize})`
+        box.addEventListener('mouseenter', () => {
+            box.style.backgroundColor = colorPicker.value
+        })
+        container.appendChild(box)
+        boxCount++
+    }
 }
 
-// Reset to re-enter While loop on next Submit
-boxCount = 0
+// Default screen
+let gridSize = 16
+drawGrid(gridSize)
 
-
+// On user input
+input.addEventListener('submit', e => {
+    e.preventDefault()
+    gridSize = parseInt(e.target[0].value)
+    // Clear out previous grid
+    container.innerHTML = ''
+    drawGrid(gridSize)
+})
