9991c4ac15113f6b41d159ac112c87c18c1b88ba
stallionblade <119752878+stallionblade@users.noreply.github.com>
2023-01-04 14:33:35 -0500

    Create randomize color option

--- script.js
+++ script.js
@@ -1,53 +1,50 @@
-const input = document.querySelector('#input')
+const newgrid = document.querySelector('#newgrid')
 const colorPicker = document.querySelector('#colorpicker')
 const container = document.querySelector('.gridcontainer')
-const randRGB = document.querySelector('#randRGB')
-let color = ""
+const randomize = document.querySelector('#randomize')
+let color = colorPicker.value
 
 // Default screen
 let gridSize = 16
-drawGrid(gridSize, color)
+drawGrid(gridSize)
 
 // On user input
-input.addEventListener('submit', (e) => {
+newgrid.addEventListener('submit', (e) => {
     e.preventDefault()
-    color = ""
     gridSize = parseInt(e.target[0].value)
     // Clear out previous grid
     container.innerHTML = ''
-    drawGrid(gridSize, color)
+    drawGrid(gridSize)
 })
 
-// On random button
-randRGB.addEventListener('click', (e) => {
-    e.preventDefault()
-    color = "random"
-    // Clear out previous grid
-    container.innerHTML = ''
-    drawGrid(gridSize, color)
-})
-
-
-function drawGrid(gridSize, color) {
-    let boxCount = 0
-    while (boxCount < Math.pow(gridSize,2)) {
-        const box = document.createElement('div')    
-        box.className = "box"
-        box.style.backgroundColor = "white"
-        box.style.borderRadius = "3px"
-        box.style.minWidth = `calc(600px/${gridSize})`
-        if (color == "random") {
-            box.addEventListener('mouseenter', () => {
-                box.style.backgroundColor = "blue"
-            })
-        }
-        else {
-            box.addEventListener('mouseenter', () => {
-                box.style.backgroundColor = colorPicker.value
-            })
-        }       
-        container.appendChild(box)
-        boxCount++
-    }
+function randomRGB() {
+    let r = Math.floor(Math.random()*256)
+    let g = Math.floor(Math.random()*256)
+    let b = Math.floor(Math.random()*256)
+    return `rgb(${r},${g},${b})`
 }
 
+function createBox() {
+    const box = document.createElement('div')    
+    box.className = "box"
+    box.style.backgroundColor = "white"
+    box.style.borderRadius = "3px"
+    box.style.minWidth = `calc(600px/${gridSize})`
+    box.addEventListener('mouseenter', () => {
+        if (randomize.checked == true) {
+            box.style.backgroundColor = randomRGB()
+        } else {
+            box.style.backgroundColor = colorPicker.value
+        }
+    })
+    return box
+}
+
+function drawGrid(gridSize) {
+    let boxCount = 0
+    while (boxCount < Math.pow(gridSize,2)) {
+        const box = createBox()
+        container.appendChild(box)
+        boxCount++;
+    }
+}
